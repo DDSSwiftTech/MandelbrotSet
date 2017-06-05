fbc3c781dd8281705bae30b04c63fd7f444d0dba
dschwartz783 <dschwartz783@gmail.com>
2017-06-04 20:17:47 -0400

    initial commit

---
+++ MandelbrotSet/Mandelbrot.swift
@@ -0,0 +1,33 @@
+//
+//  Mandelbrot.swift
+//  CGPinball
+//
+//  Created by David Schwartz on 6/4/17.
+//  Copyright © 2017 DDS Programming. All rights reserved.
+//
+
+import Foundation
+import simd
+
+class Mandelbrot {
+    func calculate(x: Double, y: Double, i: Int) -> Int {
+        
+        var z = vector2(x, y)
+        
+        for passno in 0..<i {
+            
+            if (pow(z.x, 2) + pow(z.y, 2)) > 4 {
+                return passno
+            }
+            
+            z = vector2(pow(z.x, 2) - pow(z.y, 2), 2 * z.x * z.y) - z
+        }
+        
+        return i - 1
+    }
+    
+    func pythag(x: Double, y: Double) -> Double {
+        return sqrt(x * x + y * y)
+    }
+    
+}
---
+++ MandelbrotSet/main.swift
@@ -0,0 +1,110 @@
+//
+//  main.swift
+//  MandelbrotSet
+//
+//  Created by David Schwartz on 6/4/17.
+//  Copyright © 2017 DDS Programming. All rights reserved.
+//
+
+import Cocoa
+
+let maxIter = 20
+
+func colorForColorIDX(_ idx: Int) -> CGColor {
+    return NSColor(
+        calibratedRed: CGFloat(idx)/(CGFloat(maxIter) - 1),
+        green: 1 - CGFloat(idx)/(CGFloat(maxIter) - 1),
+        blue: 1,
+        alpha: 1).cgColor
+}
+
+CGDisplayCapture(CGMainDisplayID())
+
+if let context = CGDisplayGetDrawingContext(CGMainDisplayID()) {
+    
+    let mandelBrot = Mandelbrot()
+    
+    let calcQueue = DispatchQueue(label: "calcQueue",
+                                  qos: .userInteractive,
+                                  attributes: .concurrent)
+    
+    let arrayQueue = DispatchQueue(label: "arrayQueue",
+                                   qos: .userInteractive)
+    
+    let displaySize = CGDisplayBounds(CGMainDisplayID())
+    
+    var currentColor: Int!
+    
+    context.setFillColor(colorForColorIDX(1))
+    
+    var colorArray: [Int: [CGRect]] = [:]
+    
+    for i in 0..<maxIter {
+        colorArray[i] = []
+    }
+    
+    for x in stride(from: -2, through: 2, by: 0.001) {
+        for y in stride(from: -2, through: 2, by: 0.001) {
+            
+            calcQueue.async {
+                
+                let selectedColor = mandelBrot.calculate(
+                    x: Double(x),
+                    y: Double(y),
+                    i: maxIter)
+                
+                let rect = CGRect(
+                    x: Double(displaySize.midX) + 250 * x,
+                    y: Double(displaySize.midY) + 250 * y, width: 0.1, height: 0.1)
+                
+                arrayQueue.async {
+                    colorArray[selectedColor]!.append(rect)
+                    
+                    if colorArray[selectedColor]!.count >= 10000 {
+                        context.setFillColor(colorForColorIDX(selectedColor))
+                        context.addRects(colorArray[selectedColor]!)
+                        context.fillPath()
+                        
+                        colorArray[selectedColor]!.removeAll(keepingCapacity: true)
+                    }
+                }
+            }
+        }
+    }
+    
+    calcQueue.async(flags: .barrier) {
+        arrayQueue.async(flags: .barrier) {
+            for i in 0..<maxIter {
+                context.setFillColor(colorForColorIDX(i))
+                context.addRects(colorArray[i]!)
+                context.fillPath()
+            }
+        }
+    }
+}
+
+guard let keyDownTracker = CGEvent.tapCreate(
+    tap: .cghidEventTap,
+    place: .headInsertEventTap,
+    options: .defaultTap,
+    eventsOfInterest: CGEventMask(1 << CGEventType.keyDown.rawValue),
+    callback: { (proxy, type, event, data) -> Unmanaged<CGEvent>? in
+        switch event.getIntegerValueField(.keyboardEventKeycode) {
+        case 12: // Q: Quit
+            exit(0)
+            
+        default:
+            break
+        }
+        
+        return nil
+},
+    userInfo: nil) else {
+        FileHandle.standardError.write("Could not create keyboard tap".data(using: .utf8)!)
+        exit(1)
+}
+
+RunLoop.main.add(keyDownTracker, forMode: .defaultRunLoopMode)
+
+RunLoop.main.run()
+
