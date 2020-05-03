3508b3435a44bb2bb812b15261a9ca1c4793caab
David Schwartz <dschwartz783@gmail.com>
2020-05-03 14:44:18 -0400

    Far superior performance

--- Sources/MandelbrotSet/Mandelbrot.swift
+++ Sources/MandelbrotSet/Mandelbrot.swift
@@ -14,12 +14,21 @@ extension CGVector {
     }
 }
 
+struct Vector80 {
+    let dx: Float80
+    let dy: Float80
+    
+    static func +(lhs: Vector80, rhs: Vector80) -> Vector80 {
+        return Vector80(dx: lhs.dx + rhs.dx, dy: lhs.dy + rhs.dy)
+    }
+}
+
 class Mandelbrot {
-    class func calculate(x: Double, y: Double, i: Int) -> Int {
-        var z = CGVector(dx: 0, dy: 0)
+    class func calculate(x: Float80, y: Float80, i: Int) -> Int {
+        var z = Vector80(dx: 0, dy: 0)
         
         for passno in 0..<i {
-            z = CGVector(dx: pow(z.dx, 2) - pow(z.dy, 2), dy: 2 * z.dx * z.dy) + CGVector(dx: x, dy: y)
+            z = Vector80(dx: pow(z.dx, 2) - pow(z.dy, 2), dy: 2 * z.dx * z.dy) + Vector80(dx: x, dy: y)
             
             if (pow(z.dx, 2) + pow(z.dy, 2)) >= 4 {
                 return passno
--- Sources/MandelbrotSet/main.swift
+++ Sources/MandelbrotSet/main.swift
@@ -9,8 +9,8 @@
 import Cocoa
 
 class MandelbrotDrawClass {
-    let maxIterations = 210
-    var Ox: Double = -2 {
+    let maxIterations = 20000
+    var Ox: Float80 = -2 {
         willSet {
             print("old Ox: \(Ox)")
         }
@@ -18,7 +18,7 @@ class MandelbrotDrawClass {
             print("new Ox: \(Ox)")
         }
     }
-    var Oy: Double = -2 {
+    var Oy: Float80 = -2 {
         willSet {
             print("old Oy: \(Oy)")
         }
@@ -26,7 +26,7 @@ class MandelbrotDrawClass {
             print("new Oy: \(Oy)")
         }
     }
-    var Lx: Double = 4 {
+    var Lx: Float80 = 4 {
         willSet {
             print("old Lx: \(Lx)")
         }
@@ -34,7 +34,7 @@ class MandelbrotDrawClass {
             print("new Lx: \(Lx)")
         }
     }
-    var Ly: Double = 4 {
+    var Ly: Float80 = 4 {
         willSet {
             print("old Ly: \(Ly)")
         }
@@ -43,21 +43,29 @@ class MandelbrotDrawClass {
         }
     }
     
-    let height = Int(ProcessInfo.processInfo.arguments[1]) ?? 1000
-    let width = Int(ProcessInfo.processInfo.arguments[1]) ?? 1000
+//    let height = Int(ProcessInfo.processInfo.arguments[1]) ?? 1000
+//    let width = Int(ProcessInfo.processInfo.arguments[1]) ?? 1000
     let rect: CGRect
     var randomColorList: [Int: NSColor] = [:]
     
     let saveThread = DispatchQueue(label: "savethread")
     let colorAddQueue = DispatchQueue(label: "colorAddQueue")
+    let cgContext = CGDisplayGetDrawingContext(CGMainDisplayID())!
+    let newContext: NSGraphicsContext
     
     init() {
-        rect = CGRect(
-            x: (CGFloat(self.width) - CGFloat(self.height)) / 2,
-            y: 0,
-            width: CGFloat(self.width),
-            height: CGFloat(self.height)
-        )
+        for i in 0...maxIterations {
+            self.randomColorList[i] = NSColor(
+                calibratedRed: CGFloat(arc4random()) / CGFloat(UInt32.max),
+                green: CGFloat(arc4random()) / CGFloat(UInt32.max),
+                blue: CGFloat(arc4random()) / CGFloat(UInt32.max),
+                alpha: CGFloat(arc4random()) / CGFloat(UInt32.max))
+        }
+        
+        newContext = NSGraphicsContext(cgContext: cgContext, flipped: false)
+        let tempRect = CGDisplayBounds(CGMainDisplayID())
+        rect = CGRect(x: 0, y: 0, width: tempRect.height, height: tempRect.height)
+        NSGraphicsContext.current = newContext
         
         NotificationCenter.default.addObserver(forName: NSNotification.Name(rawValue: "draw"), object: nil, queue: nil) { (aNotification) in
             
@@ -112,69 +120,60 @@ class MandelbrotDrawClass {
     }
     
     func draw() {
-        
-        // iterate through each pixel in the bitmap, and decide if it's inside the Mandelbrot set
-        // as well as how many iterations it took to leave the set
-        //            for max_iterations in 0..<self.maxIterations {
-        
-        let piece_axis_max = 100
-        let height_piece = Int(self.rect.height) / piece_axis_max
-        let width_piece = Int(self.rect.width) / piece_axis_max
-        
-        for i in 0...maxIterations {
-            self.randomColorList[i] = NSColor(
-                calibratedRed: CGFloat(arc4random()) / CGFloat(UInt32.max),
-                green: CGFloat(arc4random()) / CGFloat(UInt32.max),
-                blue: CGFloat(arc4random()) / CGFloat(UInt32.max),
-                alpha: CGFloat(arc4random()) / CGFloat(UInt32.max))
-        }
-        
-        DispatchQueue.concurrentPerform(iterations: piece_axis_max) { (piece_x) in
-            DispatchQueue.concurrentPerform(iterations: piece_axis_max) { (piece_y) in
-                autoreleasepool {
-                    let rep = NSBitmapImageRep(
-                        bitmapDataPlanes: nil,
-                        pixelsWide: width_piece,
-                        pixelsHigh: height_piece,
-                        bitsPerSample: 8,
-                        samplesPerPixel: 4,
-                        hasAlpha: true,
-                        isPlanar: false,
-                        colorSpaceName: NSColorSpaceName.deviceRGB,
-                        bytesPerRow: self.width * 4,
-                        bitsPerPixel: 32
+        autoreleasepool {
+            let offscreenRep = NSBitmapImageRep(
+                bitmapDataPlanes:nil,
+                pixelsWide:Int(self.rect.width),
+                pixelsHigh:Int(self.rect.height),
+                bitsPerSample:8,
+                samplesPerPixel:4,
+                hasAlpha:true,
+                isPlanar:false,
+                colorSpaceName:NSColorSpaceName.deviceRGB,
+                bitmapFormat:NSBitmapImageRep.Format.alphaFirst,
+                bytesPerRow:0,
+                bitsPerPixel:0
+            )!
+
+            let context = NSGraphicsContext(bitmapImageRep: offscreenRep)!
+//            NSGraphicsContext.current = context
+            let image = context.cgContext.makeImage()!
+            let nsImage = NSImage(cgImage: image, size: self.rect.size)
+            var rawTiff = nsImage.tiffRepresentation!
+            let bytes = rawTiff.withUnsafeMutableBytes { $0 }
+            
+            DispatchQueue.concurrentPerform(iterations: Int(self.rect.width)) { (x) in
+                for y in 0..<Int(self.rect.height) {
+                    let calcX = self.Ox + Float80(x) / Float80(self.rect.width) * self.Lx
+                    let calcY = self.Oy + Float80(y) / Float80(self.rect.height) * self.Ly
+                    
+                    let iterations = Mandelbrot.calculate(
+                        x: calcX,
+                        y: calcY,
+                        i: self.maxIterations
                     )
+                        
+                    let color = self.randomColorList[iterations]!
                     
-                    for x in stride(from: piece_x * width_piece, through: (piece_x + 1) * width_piece, by: 1) {
-                        for y in stride(from: piece_y * height_piece, through: (piece_y + 1) * height_piece, by: 1) {
-                            let calcX = self.Ox + Double(x) / Double(self.rect.width) * self.Lx
-                            let calcY = self.Oy + Double(y) / Double(self.rect.height) * self.Ly
-                            
-                            let iterations = Mandelbrot.calculate(
-                                x: calcX,
-                                y: calcY,
-                                i: self.maxIterations
-                            )
-                            
-                            rep?.setColor(self.randomColorList[iterations]!, atX: x % width_piece, y: y % height_piece)
-                        }
-                    }
-                    
-                    saveThread.async {
-                        try! rep!.representation(using: .png, properties: [:])?.write(to: URL(fileURLWithPath: "/Users/davidschwartz/MandelbrotPieces/mandelbrot_\(piece_y)_\(piece_x).png"))
-                    }
+                    bytes[8 + 4 * (y * Int(self.rect.height) + x)] = UInt8(color.redComponent * CGFloat(UInt8.max))
+                    bytes[9 + 4 * (y * Int(self.rect.height) + x)] = UInt8(color.greenComponent * CGFloat(UInt8.max))
+                    bytes[10 + 4 * (y * Int(self.rect.height) + x)] = UInt8(color.blueComponent * CGFloat(UInt8.max))
+                    bytes[11 + 4 * (y * Int(self.rect.height) + x)] = 0xff
                 }
             }
+            
+            let resultImage = NSImage(data: rawTiff)
+            resultImage?.draw(in: self.rect)
         }
-        
-        //        colorQueue.async(flags: .barrier) {
-        //            self.imageProcessingQueue.async(flags: .barrier) {
-        //                exit(0)
-        //            }
-        //        }
     }
 }
+
+CGDisplayCapture(CGMainDisplayID())
 
 let drawObject = MandelbrotDrawClass()
 
 drawObject.draw()
+
+RunLoop.current.add(generateKeyTracker(), forMode: .default)
+
+RunLoop.current.run()
